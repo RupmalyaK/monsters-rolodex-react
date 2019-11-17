9c1ce2af50d5d9e6f43328a0c49a8fa7b2a0f59a
RupzKoomar <rupzonthemove@gmail.com>
2019-11-17 14:26:48 +0530

    Updates

---
+++ precache-manifest.a8ea956bbd3f34e8f459c65703d98e1c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89e4f4b76489222e81347d3575f9941f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1dddd65a1511babca6ae",
+    "url": "/monsters-rolodex/static/js/2.9952ca02.chunk.js"
+  },
+  {
+    "revision": "e0668e5675beb29e95bb",
+    "url": "/monsters-rolodex/static/js/main.dde07798.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
